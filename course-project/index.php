974d28a651c5ee2cb41cb788a80a262b840d5dab
Ecarteo <ecarteocalderon2017@gmail.com>
2026-02-18 17:33:03 -0500

    Main website base to start

---
+++ course-project/index.php
@@ -0,0 +1,22 @@
+<?php
+// Connect to the database
+include "db.php";
+
+// Get all queries from the database
+$query = "SELECT *  FROM resumes";
+$result = mysqli_query($conn, $query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume Builder</title>
+    <!-- Bootstrap CSS: https://getbootstrap.com/docs/5.3/getting-started/download/ -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
