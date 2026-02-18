522d9052dbb5040e4602e97aad697a3366d2bdfa
Ecarteo <ecarteocalderon2017@gmail.com>
2026-02-18 18:16:48 -0500

    Create.php base added

--- course-project/index.php
+++ course-project/index.php
@@ -18,41 +18,41 @@ $result = mysqli_query($conn, $query);
 </head>
 <body>
 
-<!-- Website structure  -->
-<!-- -Classes' only purpose is to use Bootstrap features. -->
-<div class="container mt-5">
-    <h1 class="mb-4">Resume Builder</h1>
-    <a href="create.php" class="btn btn-primary mb-3">Add New Resume</a>
+    <!-- Website structure  -->
+    <!-- -Classes' only purpose is to use Bootstrap features. -->
+    <div class="container mt-5">
+        <h1 class="mb-4">Resume Builder</h1>
+        <a href="create.php" class="btn btn-primary mb-3">Add New Resume</a>
 
-    <table class="table table-bordered table-striped">
-        <thead class="table-dark">
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($row = mysqli_fetch_assoc($result)) { ?>
-            <tr>
-                <td><?php echo $row['first_name'] . " " . $row['last_name']; ?></td>
-                <td><?php echo $row['current_position']; ?></td>
-                <td><?php echo $row['email']; ?></td>
-                <td><?php echo $row['phone']; ?></td>
-                <td>
-                    <a href="update.php?id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
-                    <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
-                </td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-</div>
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($row = mysqli_fetch_assoc($result)) { ?>
+                <tr>
+                    <td><?php echo $row['first_name'] . " " . $row['last_name']; ?></td>
+                    <td><?php echo $row['current_position']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['phone']; ?></td>
+                    <td>
+                        <a href="update.php?id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
+                        <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 
-<!-- Bootstrap JS: https://getbootstrap.com/docs/5.3/getting-started/download/ -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS: https://getbootstrap.com/docs/5.3/getting-started/download/ -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </body>
 </html>
