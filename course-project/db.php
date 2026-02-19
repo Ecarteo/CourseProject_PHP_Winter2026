b8b80fe21714818443afe04063f691cf36a1d6d5
Ecarteo <ecarteocalderon2017@gmail.com>
2026-02-18 21:30:31 -0500

    Changed DB data to match server's settings.

--- course-project/db.php
+++ course-project/db.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "resume_builder";
+$host = "sql213.infinityfree.com";
+$user = "if0_41192534";
+$password = "0w9Lye6zrpSdf2w";
+$database = "if0_41192534_resume_builder";
 
 // Create connection
 $conn = mysqli_connect($host, $user, $password, $database);
