ae82ad489f00f922fae3439004d7fc408c0ca6f8
Ecarteo <ecarteocalderon2017@gmail.com>
2026-02-18 21:54:16 -0500

    Added favicon and finished project's first phase.

--- course-project/index.php
+++ course-project/index.php
@@ -13,6 +13,7 @@ $result = mysqli_query($conn, $query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resume Builder</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <!-- Bootstrap CSS: https://getbootstrap.com/docs/5.3/getting-started/download/ -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
